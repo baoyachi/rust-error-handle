c1fb362296c27f2e0351828b82177e79f3d42bc9
baoyachi <liaoymxsdl@gmail.com>
2020-02-28 13:14:51 +0800

    add file

--- src/demo_03_read_file.rs
+++ src/demo_03_read_file.rs
@@ -9,8 +9,6 @@ mod tests {
     #[test]
     fn test_read_file() {
         let path = "/tmp/dat";
-
-        ///panic error:
         ///called `Result::unwrap()` on an `Err` value: Os { code: 2, kind: NotFound, message: "No such file or directory" }
         println!("{}", read_file(path));
     }
