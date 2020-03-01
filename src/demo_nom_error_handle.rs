ad53a504716fa027a75d65272989d53ea9f06b55
baoyachi <liaoymxsdl@gmail.com>
2020-03-01 18:55:27 +0800

    update test

--- src/demo_nom_error_handle.rs
+++ src/demo_nom_error_handle.rs
@@ -5,22 +5,149 @@ use nom::bytes::complete::tag;
 use nom::combinator::map;
 use nom::character::complete::alphanumeric1;
 use time::PrimitiveDateTime;
+use time::ParseError as TimeParseError;
+use std::io::Error as IoError;
+use std::error::Error as StdError;
+use std::fmt::Display;
+use nom::lib::std::fmt::Formatter;
+use nom::error::ParseError;
+
+pub type Result<I> = std::result::Result<I, Error<I>>;
+pub type IResult<I, O> = std::result::Result<(I, O), Error<I>>;
 
 
-//1. read file
-fn read_file(path: String) -> Vec<String> {
-    let content = fs::read_to_string(path).unwrap();
-    let mut lines = Vec::new();
-    for line in content.lines() {
-        if !line.clone().trim().is_empty() {
-            lines.push(line.to_string())
-        }
-    }
-    lines
+#[derive(Debug)]
+pub struct Error<I> {
+    kind: ErrorKind,
+    error: nom::error::VerboseError<I>,
+    incomplete: String,
+
 }
 
-//2. nom parser
-fn parser_time(input: &str) -> nom::IResult<&str, (String, String)> {
+impl<I> Error<I> {
+    pub fn new(kind: ErrorKind) -> Error<I> {
+        Error {
+            kind,
+            error: nom::error::VerboseError { errors: vec![] },
+            incomplete: "".to_string(),
+        }
+    }
+
+    pub fn new_nom_error(input: I, kind: nom::error::ErrorKind) -> Self {
+        Self {
+            kind: ErrorKind::NomParserError("Nom parser error".to_string()),
+            error: nom::error::VerboseError::from_error_kind(input, kind),
+            incomplete: "".to_string(),
+        }
+    }
+}
+
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    IoError(IoError),
+    TimeParseError(TimeParseError),
+    NomParserError(String),
+    StringError(String),
+    EmptyError,
+}
+
+impl<I: std::fmt::Debug> StdError for Error<I> {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match &self.kind {
+            ErrorKind::IoError(ref e) => Some(e),
+            ErrorKind::TimeParseError(ref e) => Some(e),
+            ErrorKind::NomParserError(_) => None,
+            ErrorKind::StringError(_) => None,
+            ErrorKind::EmptyError => None,
+        }
+    }
+}
+
+impl<I> Display for Error<I> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.kind {
+            ErrorKind::IoError(ref e) => e.fmt(f),
+            ErrorKind::TimeParseError(ref e) => e.fmt(f),
+            ErrorKind::NomParserError(ref e) => e.fmt(f),
+            ErrorKind::StringError(ref e) => e.fmt(f),
+            ErrorKind::EmptyError => write!(f, "Empty Error"),
+        }
+    }
+}
+
+impl<I> From<TimeParseError> for Error<I> {
+    fn from(e: TimeParseError) -> Self {
+        Error::new(ErrorKind::TimeParseError(e))
+    }
+}
+
+impl<I> From<std::string::String> for Error<I> {
+    fn from(s: std::string::String) -> Self {
+        Error::new(ErrorKind::StringError(s.to_string()))
+    }
+}
+
+
+impl<I> From<IoError> for Error<I> {
+    fn from(s: IoError) -> Self {
+        Error::new(ErrorKind::IoError(s))
+    }
+}
+
+impl<I: std::fmt::Debug> From<nom::Err<(I, nom::error::ErrorKind)>> for Error<I> {
+    fn from(i: nom::Err<(I, nom::error::ErrorKind)>) -> Self {
+        match i {
+            nom::Err::Error(err) | nom::Err::Failure(err) => Error::new_nom_error(err.0, err.1),
+            nom::Err::Incomplete(i) => Error::new(ErrorKind::StringError(format!(
+                "Nom parser Incomplete error: {:?}",
+                i
+            ))),
+        }
+    }
+}
+
+
+impl<I> Into<nom::error::VerboseError<I>> for Error<I> {
+    fn into(self) -> nom::error::VerboseError<I> {
+        self.error
+    }
+}
+
+impl<I: std::fmt::Debug> nom::error::ParseError<I> for Error<I> {
+    fn from_error_kind(input: I, kind: nom::error::ErrorKind) -> Self {
+        Error::new_nom_error(input, kind)
+    }
+
+    fn append(input: I, kind: nom::error::ErrorKind, mut other: Self) -> Self {
+        other
+            .error
+            .errors
+            .push((input, nom::error::VerboseErrorKind::Nom(kind)));
+        other
+    }
+
+    fn from_char(input: I, c: char) -> Self {
+        Self {
+            kind: ErrorKind::EmptyError,
+            error: nom::error::VerboseError::from_char(input, c),
+            incomplete: "".to_string(),
+        }
+    }
+
+    fn add_context(input: I, ctx: &'static str, mut other: Self) -> Self {
+        other
+            .error
+            .errors
+            .push((input, nom::error::VerboseErrorKind::Context(ctx)));
+        other
+    }
+}
+
+
+
+// nom parser:2020/03/01 15:30:22 -> 2020-03-01 15:30:22
+fn parser_time(input: &str) -> IResult<&str, (String, String)> {
     //"2020/03/01 15:30:22" or "  2020/03/01 15:30:22       "
     let time_value = tuple((
         tag("\""),
@@ -55,15 +182,11 @@ fn parser_time(input: &str) -> nom::IResult<&str, (String, String)> {
     Ok((input, (y1.to_string(), time)))
 }
 
-//3. parser time
-fn get_time(path: String) -> Vec<(String, PrimitiveDateTime)> {
-    let mut times = Vec::new();
-    for line in read_file(path) {
-        let (_, (key, value)) = parser_time(line.as_str()).unwrap();
-        let time = PrimitiveDateTime::parse(value, "%F %T").unwrap();
-        times.push((key, time))
-    }
-    times
+// parser time
+fn get_time<'a>(content: &'a str) -> IResult<&'a str, (String, PrimitiveDateTime)> {
+    let (input, (key, value)) = parser_time(content)?;
+    let time = PrimitiveDateTime::parse(value, "%F %T")?;
+    Ok((input, (key, time)))
 }
 
 #[cfg(test)]
@@ -85,9 +208,15 @@ mod tests {
     #[test]
     fn test_convert_time() {
         let path = "./time_tpl.file";
-        for (key,value) in get_time(path.to_string()){
-            assert_eq!(key,"time");
-            assert_eq!(value,PrimitiveDateTime::new(date!(2020-03-01), time!(15:30:22)));
+        let content = fs::read_to_string(path).unwrap();
+        for line in content.clone().lines().into_iter() {
+            if line.clone().trim().is_empty() {
+                continue;
+            }
+            let (input, (key, value)) = get_time(line).unwrap();
+            assert_eq!(input, "");
+            assert_eq!(key, "time");
+            assert_eq!(value, PrimitiveDateTime::new(date!(2020-03-01), time!(15:30:22)));
         }
     }
 }
